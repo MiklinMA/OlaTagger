6afcf427f213191edd5f623e06a7d420dabcda71
Mike Miklin <MiklinMA@gmail.com>
2024-02-21 12:52:49 +0500

    Cosmetic

--- Package.swift
+++ Package.swift
@@ -9,15 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OlaTagger",
-            targets: ["OlaTagger"]),
+            targets: ["OlaTagger"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "OlaTagger"),
+        .target(name: "OlaTagger"),
         .testTarget(
             name: "OlaTaggerTests",
-            dependencies: ["OlaTagger"]),
+            dependencies: ["OlaTagger"]
+        ),
     ]
 )
